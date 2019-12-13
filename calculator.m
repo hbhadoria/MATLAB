c28c0a655613cc8b59dd234322b718b8f67a3df3
hbhadoria <hemant.bhadoria09@gmail.com>
2019-12-13 13:38:10 +0530

    Add files via upload
    small calculator project

---
+++ calculator.m
@@ -0,0 +1,272 @@
+function varargout = calculator(varargin)
+% CALCULATOR MATLAB code for calculator.fig
+%      CALCULATOR, by itself, creates a new CALCULATOR or raises the existing
+%      singleton*.
+%
+%      H = CALCULATOR returns the handle to a new CALCULATOR or the handle to
+%      the existing singleton*.
+%
+%      CALCULATOR('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in CALCULATOR.M with the given input arguments.
+%
+%      CALCULATOR('Property','Value',...) creates a new CALCULATOR or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before calculator_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to calculator_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help calculator
+
+% Last Modified by GUIDE v2.5 07-Oct-2019 23:32:36
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @calculator_OpeningFcn, ...
+                   'gui_OutputFcn',  @calculator_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before calculator is made visible.
+function calculator_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to calculator (see VARARGIN)
+
+% Choose default command line output for calculator
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes calculator wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = calculator_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in zero.
+function zero_Callback(hObject, eventdata, handles)
+% hObject    handle to zero (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+existingstring=get(handles.text1,'string');
+newstring=('0');
+textstring=strcat(existingstring,newstring);
+set(handles.text1,'string',textstring);
+
+
+% --- Executes on button press in four.
+function four_Callback(hObject, eventdata, handles)
+% hObject    handle to four (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+existingstring=get(handles.text1,'string');
+newstring=('4');
+textstring=strcat(existingstring,newstring);
+set(handles.text1,'string',textstring);
+
+
+% --- Executes on button press in eight.
+function eight_Callback(hObject, eventdata, handles)
+% hObject    handle to eight (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+existingstring=get(handles.text1,'string');
+newstring=('8');
+textstring=strcat(existingstring,newstring);
+set(handles.text1,'string',textstring);
+
+
+% --- Executes on button press in multiply.
+function multiply_Callback(hObject, eventdata, handles)
+% hObject    handle to multiply (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+existingstring=get(handles.text1,'string');
+newstring=('*');
+textstring=strcat(existingstring,newstring);
+set(handles.text1,'string',textstring);
+
+
+% --- Executes on button press in one.
+function one_Callback(hObject, eventdata, handles)
+% hObject    handle to one (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+existingstring=get(handles.text1,'string');
+newstring=('1');
+textstring=strcat(existingstring,newstring);
+set(handles.text1,'string',textstring);
+
+
+% --- Executes on button press in five.
+function five_Callback(hObject, eventdata, handles)
+% hObject    handle to five (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+existingstring=get(handles.text1,'string');
+newstring=('5');
+textstring=strcat(existingstring,newstring);
+set(handles.text1,'string',textstring);
+
+
+% --- Executes on button press in nine.
+function nine_Callback(hObject, eventdata, handles)
+% hObject    handle to nine (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+existingstring=get(handles.text1,'string');
+newstring=('9');
+textstring=strcat(existingstring,newstring);
+set(handles.text1,'string',textstring);
+
+
+% --- Executes on button press in division.
+function division_Callback(hObject, eventdata, handles)
+% hObject    handle to division (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+existingstring=get(handles.text1,'string');
+newstring=('/');
+textstring=strcat(existingstring,newstring);
+set(handles.text1,'string',textstring);
+
+
+% --- Executes on button press in two.
+function two_Callback(hObject, eventdata, handles)
+% hObject    handle to two (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+existingstring=get(handles.text1,'string');
+newstring=('2');
+textstring=strcat(existingstring,newstring);
+set(handles.text1,'string',textstring);
+
+
+% --- Executes on button press in six.
+function six_Callback(hObject, eventdata, handles)
+% hObject    handle to six (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+existingstring=get(handles.text1,'string');
+newstring=('6');
+textstring=strcat(existingstring,newstring);
+set(handles.text1,'string',textstring);
+
+
+% --- Executes on button press in minus.
+function minus_Callback(hObject, eventdata, handles)
+% hObject    handle to minus (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+existingstring=get(handles.text1,'string');
+newstring=('-');
+textstring=strcat(existingstring,newstring);
+set(handles.text1,'string',textstring);
+
+
+% --- Executes on button press in clear.
+function clear_Callback(hObject, eventdata, handles)
+% hObject    handle to clear (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+existingstring=get(handles.text1,'string');
+newstring=('');
+set(handles.text1,'string',newstring);
+
+
+% --- Executes on button press in three.
+function three_Callback(hObject, eventdata, handles)
+% hObject    handle to three (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+existingstring=get(handles.text1,'string');
+newstring=('3');
+textstring=strcat(existingstring,newstring);
+set(handles.text1,'string',textstring);
+
+
+% --- Executes on button press in seven.
+function seven_Callback(hObject, eventdata, handles)
+% hObject    handle to seven (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+existingstring=get(handles.text1,'string');
+newstring=('7');
+textstring=strcat(existingstring,newstring);
+set(handles.text1,'string',textstring);
+
+
+% --- Executes on button press in plus.
+function plus_Callback(hObject, eventdata, handles)
+% hObject    handle to plus (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+existingstring=get(handles.text1,'string');
+newstring=('+');
+textstring=strcat(existingstring,newstring);
+set(handles.text1,'string',textstring);
+
+
+% --- Executes on button press in equal.
+function equal_Callback(hObject, eventdata, handles)
+% hObject    handle to equal (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+existingstring=get(handles.text1,'string');
+result=eval(existingstring);
+
+set(handles.text1,'string',result);
+
+
+
+
+function text1_Callback(hObject, eventdata, handles)
+% hObject    handle to text1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of text1 as text
+%        str2double(get(hObject,'String')) returns contents of text1 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function text1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to text1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
